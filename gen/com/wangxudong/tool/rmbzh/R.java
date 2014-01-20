2fd9648c170ca05d61a7d1988271e8dd69f93f06
wxd56 <wxd56@126.com>
2014-01-20 21:05:28 +0800

    12

--- gen/com/wangxudong/tool/rmbzh/R.java
+++ gen/com/wangxudong/tool/rmbzh/R.java
@@ -26,7 +26,9 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int main_background=0x7f020001;
+        public static final int icon=0x7f020001;
+        public static final int main_background=0x7f020002;
+        public static final int show_result_background=0x7f020003;
     }
     public static final class id {
         public static final int big_number=0x7f060003;
